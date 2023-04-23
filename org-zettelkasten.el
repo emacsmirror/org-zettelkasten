50c092f0a22981e2e447dd421aa474fbc9b384ba
Yann Herklotz <git@yannherklotz.com>
2023-04-23 12:35:20 +0100

    Update version for prerelease

--- org-zettelkasten.el
+++ org-zettelkasten.el
@@ -8,7 +8,7 @@
 ;; Homepage: https://sr.ht/~ymherklotz/org-zettelkasten
 ;; Package-Requires: ((emacs "25.1") (org "9.3"))
 
-;; Version: 0.7.0
+;; Version: 0.8.0
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
